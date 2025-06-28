4642d8e3efcdfefbdf568017b24a1c77edbbe02d
Gustavo Alberto <83519205+gustavo-alberto@users.noreply.github.com>
2025-06-27 21:15:35 -0300

    feat: add entrypoint script
    allows set dev or prod mode in .env file and run custom entrypoint script

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e  # Exit immediately if a command exits with a non-zero status
+
+echo "Running in mode: $MODE"
+
+# Apply database migrations before starting the server
+python manage.py migrate
+
+if [ "$MODE" = "dev" ]; then
+  echo "Starting Django development server (runserver)..."
+  python manage.py runserver 0.0.0.0:8000
+
+elif [ "$MODE" = "prod" ]; then
+  echo "Starting uWSGI server (production mode)..."
+  uwsgi --http :8000 --module core.wsgi --chmod-socket=666
+
+else
+  echo "Unknown mode: $MODE"
+  exit 1
+fi
